95ad8816bb3f505903cfb991a2123f93bab4305c
Max Andriushchenko <m.andriushchenko@gmail.com>
2016-05-25 21:13:55 +0300

    Prototype version is coming

--- src/lookup.py
+++ src/lookup.py
@@ -1,20 +1,33 @@
 import json
 import requests
+import nltk
+from microsofttranslator import Translator
 
 
-def lookup_keyword(string, cls, type_, max_hits=1):
+def translate(string, to_lang='en'):
+    translator = Translator('max-andr', '36OSL0SDYEtJCS1Z9kmDvbXkaOFeriDcB2ZvLSAA+q8=')
+    return translator.translate(string, to_lang)
+
+
+def lookup_keyword(string, cls='', type_='Keyword', max_hits=1):
     url = 'http://lookup.dbpedia.org/api/search/' + type_ + 'Search'
     params = {'QueryString': string,
-              'QueryClass': cls,
-              'MaxHits': max_hits}
+              'QueryClass':  cls,
+              'MaxHits':     max_hits}
     headers = {'Accept': 'application/json'}
-    resp = json.loads(requests.get(url=url, params=params, headers=headers).text)
+    resp = json.loads(requests.
+                      get(url=url, params=params, headers=headers).
+                      text)
     if resp['results']:
-        name = resp['results'][0]['label']
-        description = resp['results'][0]['description']
-        return name, description
+        res = resp['results'][0]
+        uri = res['uri']
+        name = res['label']
+        description = res['description']
+        classes = [cls['uri'] for cls in res['classes']]
+        return uri, name, description, classes
     else:
-        raise Exception('No results for <{0}> as <{1}Search>'.format(string, type_))
+        print('No results for <{0}> of class <{1}> (<{2}Search>)'.
+              format(string, cls, type_))
 
 
 def print_autocomplete(string, cls):
@@ -25,6 +38,26 @@ def print_autocomplete(string, cls):
 
 string, cls, type_ = 'Dnipropetrovsk', 'place', 'Prefix'
 lookup_keyword(string, cls, type_)
+lookup_keyword('Dnipropetrovsk')
+lookup_keyword('Abraham Lincoln')
+Agent -> Person -> OfficeHolder
+get all relations for given CLASS (to restrict possible reasoning)
+
+использовать ontology:comment при матчинге вопроса и свойства (TF-IDF)
+ссылки на онтологию (описания entity) разные. использовать только DBPedia?
+
+ontology описывает entity: Class, ObjectProperty, ...
+rdfs:domain - область допустимых свойств
+rdfs:range - область допустимых значений
+подготовить это всё в виде теоретической части
+
+lookup_keyword('Авраам Линкольн')
+
+question = 'В какой стране родился Линкольн?'
+question = 'Чем знаменит Линкольн?'
+question = 'Кто был убит в Вашингтоне?'
+
+
 # for key in resp['results'][0].keys():
 #     print(key)
 #     print(resp['results'][0][key])
@@ -32,7 +65,12 @@ lookup_keyword(string, cls, type_)
 print_autocomplete('Dnipropetrovsk', '')
 print_autocomplete('Киев', '')
 
-# TODO: translate api?
+question = translate('Где находится Днепропетровск?')
+print(question)
 
 
+for token in nltk.word_tokenize(question):
+    # only NOUNs
+    found = lookup_keyword(token)
+    print(token, '\n', found, '\n')
 
--- src/qa.py
+++ src/qa.py
@@ -1,18 +1,17 @@
 import re
 import json
 import requests
+import pymorphy2
 import pprint
 import pandas as pd
 from io import StringIO
+from sklearn.feature_extraction.text import CountVectorizer
 
 pd.set_option('display.large_repr', 'truncate')
 # tables in 1 line
 # pd.set_option('display.max_columns', 0)
 # set width of 1 column
 pd.set_option('max_colwidth', 35)
-
-# import pymorphy2 as morph
-# analyzer = morph.MorphAnalyzer()
 
 
 def query_sparql(query, format_='csv'):
@@ -26,34 +25,61 @@ def query_sparql(query, format_='csv'):
     }
     if format_ == 'csv':
         data = requests.get(url=url, params=params).text
-    elif format_ == 'json':
-        data = json.loads(requests.get(url=url, params=params).text)
+    elif format_ in ('json',):
+        string = requests.get(url=url, params=params).text
+        data = json.loads(string)
     else:
         raise Exception('Response format is wrong.')
     # explicit error handling
-    if re.findall("Virtuoso \d+ Error", data):
+    if re.findall("Virtuoso \d+ Error", str(data)):
         raise Exception(data)
     # pprint.pprint(data)
     return data
 
 
+def get_property_descr(prop_url):
+    rdfs_descr = {'label':   'http://www.w3.org/2000/01/rdf-schema#label',
+                  'comment': 'http://www.w3.org/2000/01/rdf-schema#comment'}
+    r_json = query_sparql('DESCRIBE <{0}>'.format(prop_url), format_='json')
+    descr = ''
+    for spo in r_json['results']['bindings']:
+        # if the given predicate is description (label or comment for ontology/property)
+        if spo['p']['value'] in rdfs_descr.values():
+            descr += spo['o']['value'] + '\n'
+    return descr
+
+
 query = """
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX dbprop: <http://dbpedia.org/property/>
-SELECT distinct *
-WHERE {
-    dbprop:Гранд_Макет_Россия
-    ?dbpedia_url rdf:type dbo:Place .
-    ?dbpedia_url rdfs:label ?name .
-    FILTER(lang(?name) = 'ru')
-}
-LIMIT 20
+select distinct ?property, ?subject, ?obj
+where {{
+     ?subject a <{0}> .
+     ?subject ?property ?obj .
+     FILTER(?subject=<{1}>)
+}}
+limit 200000
 """
+# values ?target {dbpedia:Dnepr}
+
 csv_response = query_sparql(query, format_='csv')
 df = pd.read_csv(StringIO(csv_response))
-
 print(df)
 
+
+
+# http://dbpedia.org/property/timezone
+uri = lookup_keyword('Dnipropetrovsk')[0]
+# TODO: определять Place or Settlement or ... через Lookup
+
+query_with_values = query.format('http://dbpedia.org/ontology/Place', uri)
+r_json = query_sparql(query_with_values, format_='json')
+pprint.pprint(r_json)
+# TODO: по каждому property получить описание функцией get_property_descr
+подлежащее - тоже класс с разными методами
+property/ontology сделать как класс с методом получения описания
+sparql и лукап запросы в модуль dbpedia_api
+
+prop_descr = get_property_descr('http://dbpedia.org/property/julPrecipitationMm')
+print(prop_descr)
 
 # TODO: natural_language -> syntax parsed format -> SPARQL
 # TODO: Named Entity Recognition! Find out, what word is central?
@@ -66,3 +92,37 @@ print(df)
 # "birth place": "London, UK"
 
 # TODO: question type classification
+
+question = 'В какой стране родился Линкольн?'
+birthPlace = ?, entity = Lincoln
+
+1. найти сказуемое (глагол)
+2. edit distance: birth -> birthPlace
+# question = 'В какой стране живёт Линкольн?'
+# question = 'В какой стране находится Днепропетровск?'
+q_constructions = {'в каком году':   ['year'],
+                   'в каком городе': ['place'],
+                   'в какой стране': ['country'],
+                   'где находится':  ['place', 'country'],
+                   '':               ''}
+q_verb = {'родиться':   ['birthPlace'],
+          'находиться': ['situatedIn'],
+          'жить':       ['livesIn']}
+
+morph = pymorphy2.MorphAnalyzer()
+vect = CountVectorizer(ngram_range=(1, 3))
+vect.fit_transform([question])
+
+# TODO: токенайзер придумать (на почте)
+ngrams = vect.get_feature_names()
+print(ngrams)
+
+for ngram in [ngram for ngram in ngrams if len(ngram.split()) == 1]:
+    pos = morph.parse(ngram)[0].tag.POS
+    print(ngram, '__', pos)
+
+# TODO: 2 большие буквы
+# TODO: multi-class classification по каждому вопросу;
+# предсказать relationType (bornIn, livesIn, aKindOf);
+# обучаться на Википедии; ... born in ... - предложение with positive label.
+
